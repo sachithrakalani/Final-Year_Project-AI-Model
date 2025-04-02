9a362301202e3d0b3b0961422390fdf2ac7136d2
sachithrakalani <kalanisathyangi@gmail.com>
2025-04-02 11:56:26 +0530

    Load the dataset using pandas Python library

--- train.py
+++ train.py
@@ -2,9 +2,12 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import os
+import pandas as pd
 
 nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('punkt_tab')
 
-os.makedirs('models', exist_ok=True)
+os.makedirs('models', exist_ok=True)\
+
+data = pd.read_csv("data/deceptive-opinion.csv")
