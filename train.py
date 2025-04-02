75b0a4f598c5f52c78d88dc47c110294db6caf7f
sachithrakalani <kalanisathyangi@gmail.com>
2025-04-02 11:50:25 +0530

    Import os module

--- train.py
+++ train.py
@@ -1,7 +1,10 @@
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+import os
 
 nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('punkt_tab')
+
+os.makedirs('models', exist_ok=True)
