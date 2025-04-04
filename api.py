12230a19c3e1604b0521b10b2f693330297dff95
sachithrakalani <kalanisathyangi@gmail.com>
2025-04-04 11:22:28 +0530

    Prediction function

--- api.py
+++ api.py
@@ -16,19 +16,6 @@ nltk.download('punkt_tab')
 model = joblib.load('models/fake_review_model.pkl')
 vectrorizer = joblib.load('models/tfidf_vectorizer.pkl')
 
-# # Text preprocessing function
-# def clean_text(text):
-#     if not isinstance(text, str):  # Handle non-string inputs'
-#         return ""
-#     text = text.lower()  # Convert to lowercase
-#     text = re.sub(r'\W', ' ', text)  # Remove special characters
-#     text = re.sub(r'\s+', ' ', text)  # Remove extra spaces
-#     text = re.sub(r'\d+', '', text)  # Remove numbers
-#     words = word_tokenize(text)  # Tokenize the text
-#     stop_words = set(stopwords.words('english'))  # Load stopwords
-#     words = [word for word in words if word not in stop_words]  # Remove stopwords
-#     return ' '.join(words)
-
 def clean_text(text):
     if not isinstance(text,str):
         return " "
@@ -40,3 +27,10 @@ def clean_text(text):
     stop_words = set(stopwords.words('english'))
     words = [word for word in words if word not in stop_words]
     return ' '.join(words)
+
+def predict_review(review):
+    cleaned_review = clean_text(review)
+    vectrorized_review = vectrorizer.transform([cleaned_review]).toarray()
+    prediction = model.predict(vectrorized_review)
+    return "Fake Review" if prediction[0] == 1 else "Real Review"
+
