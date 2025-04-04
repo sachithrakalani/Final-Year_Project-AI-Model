b529308cdc0616a30811a4117b47f9f627e2bc36
sachithrakalani <kalanisathyangi@gmail.com>
2025-04-04 10:29:09 +0530

    Load the trained model and vectorizer

--- api.py
+++ api.py
@@ -5,9 +5,14 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
+from train import vectrorizer
+
 app = Flask(__name__)
 
 nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('punkt_tab')
+
+model = joblib.load('models/fake_review_model.pkl')
+vectrorizer = joblib.load('models/tfidf_vectorizer.pkl')
 
