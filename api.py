b556a4189f35056de79b2c3006b9b064d1a95543
sachithrakalani <kalanisathyangi@gmail.com>
2025-04-04 11:47:39 +0530

    Run the app using python api.py

--- api.py
+++ api.py
@@ -5,8 +5,6 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-from train import vectrorizer
-
 app = Flask(__name__)
 
 nltk.download('stopwords')
@@ -14,29 +12,29 @@ nltk.download('punkt')
 nltk.download('punkt_tab')
 
 model = joblib.load('models/fake_review_model.pkl')
-vectrorizer = joblib.load('models/tfidf_vectorizer.pkl')
+vectorizer = joblib.load('models/tfidf_vectorizer.pkl')
 
 def clean_text(text):
-    if not isinstance(text,str):
-        return " "
-    text = text.lower()
-    text = re.sub(r'\W', ' ',text)
-    text = re.sub(r'\s+', ' ', text)
-    text = re.sub(r'\d+', ' ', text)
-    words = word_tokenize(text)
-    stop_words = set(stopwords.words('english'))
-    words = [word for word in words if word not in stop_words]
+    if not isinstance(text, str):  # Handle non-string inputs
+        return ""
+    text = text.lower()  # Convert to lowercase
+    text = re.sub(r'\W', ' ', text)  # Remove special characters
+    text = re.sub(r'\s+', ' ', text)  # Remove extra spaces
+    text = re.sub(r'\d+', '', text)  # Remove numbers
+    words = word_tokenize(text)  # Tokenize the text
+    stop_words = set(stopwords.words('english'))  # Load stopwords
+    words = [word for word in words if word not in stop_words]  # Remove stopwords
     return ' '.join(words)
 
 def predict_review(review):
     cleaned_review = clean_text(review)
-    vectrorized_review = vectrorizer.transform([cleaned_review]).toarray()
-    prediction = model.predict(vectrorized_review)
+    vectorized_review = vectorizer.transform([cleaned_review]).toarray()
+    prediction = model.predict(vectorized_review)
     return "Fake Review" if prediction[0] == 1 else "Real Review"
 
-@app .route('predict', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    data =request.get_json()
+    data = request.get_json()
     if not data or 'review' not in data:
         return jsonify({'error': 'No review provided'}), 400
 
@@ -44,3 +42,5 @@ def predict():
     prediction = predict_review(review)
     return jsonify({'review': review, 'prediction': prediction})
 
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=3000)
