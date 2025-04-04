8629622407dad7e735ac2943d35827b7b1084940
sachithrakalani <kalanisathyangi@gmail.com>
2025-04-04 11:17:11 +0530

    Text preprocessing function

--- api.py
+++ api.py
@@ -16,3 +16,27 @@ nltk.download('punkt_tab')
 model = joblib.load('models/fake_review_model.pkl')
 vectrorizer = joblib.load('models/tfidf_vectorizer.pkl')
 
+# # Text preprocessing function
+# def clean_text(text):
+#     if not isinstance(text, str):  # Handle non-string inputs'
+#         return ""
+#     text = text.lower()  # Convert to lowercase
+#     text = re.sub(r'\W', ' ', text)  # Remove special characters
+#     text = re.sub(r'\s+', ' ', text)  # Remove extra spaces
+#     text = re.sub(r'\d+', '', text)  # Remove numbers
+#     words = word_tokenize(text)  # Tokenize the text
+#     stop_words = set(stopwords.words('english'))  # Load stopwords
+#     words = [word for word in words if word not in stop_words]  # Remove stopwords
+#     return ' '.join(words)
+
+def clean_text(text):
+    if not isinstance(text,str):
+        return " "
+    text = text.lower()
+    text = re.sub(r'\W', ' ',text)
+    text = re.sub(r'\s+', ' ', text)
+    text = re.sub(r'\d+', ' ', text)
+    words = word_tokenize(text)
+    stop_words = set(stopwords.words('english'))
+    words = [word for word in words if word not in stop_words]
+    return ' '.join(words)
