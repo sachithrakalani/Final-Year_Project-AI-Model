b1a2c817224005310e2d4e321be4bcba461bc9e7
sachithrakalani <kalanisathyangi@gmail.com>
2025-04-04 10:25:58 +0530

    Import Flask, request, jsonify, joblib, re and nltk libraries

---
+++ api.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+import joblib
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+app = Flask(__name__)
+
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('punkt_tab')
+
